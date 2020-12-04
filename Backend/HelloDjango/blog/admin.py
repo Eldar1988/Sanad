435ac4e90bd8116a09774d5ca601af0bf8c10ff0
Хайбулов Эльдар <64076865+Eldar1988@users.noreply.github.com>
2020-12-04 22:24:32 +0600

    Админка для приложения Blog

--- Backend/HelloDjango/blog/admin.py
+++ Backend/HelloDjango/blog/admin.py
@@ -1,3 +1,64 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
-# Register your models here.
+from .models import Video, Image, Post, MedicalHistory, PostReviews, MedicalHistoryReviews
+
+admin.site.register(PostReviews)
+admin.site.register(MedicalHistoryReviews)
+
+
+@admin.register(Video)
+class VideoAdmin(admin.ModelAdmin):
+    list_display = ('title', 'order', 'pub_date', 'update')
+    list_editable = ('order',)
+    search_fields = ('title',)
+    list_filter = ('pub_date', 'update')
+    save_as = True
+    save_on_top = True
+
+
+@admin.register(Image)
+class AdminImage(admin.ModelAdmin):
+    list_display = ('title', 'order', 'pub_date', 'update')
+    list_editable = ('order',)
+    search_fields = ('title',)
+    list_filter = ('pub_date', 'update')
+    save_as = True
+    save_on_top = True
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('get_image', 'title', 'doctor', 'direction', 'is_action', 'is_news', 'public_on_home_page',
+                    'slug', 'order', 'views', 'pub_date', 'update')
+    list_editable = ('doctor', 'direction', 'is_action', 'is_news', 'public_on_home_page', 'slug', 'order')
+    list_display_links = ('get_image', 'title')
+    search_fields = ('title',)
+    list_filter = ('doctor', 'direction', 'is_action', 'is_news', 'public_on_home_page', 'pub_date', 'update')
+    save_as = True
+    save_on_top = True
+
+    def get_image(self, obj):
+        return mark_safe(f'<img src="{obj.image}" height=50')
+
+    get_image.short_description = 'Фото'
+
+
+@admin.register(MedicalHistory)
+class MedicalHistoryAdmin(admin.ModelAdmin):
+    list_display = ('get_image', 'title', 'doctor', 'direction', 'public_on_home_page',
+                    'slug', 'order', 'views', 'pub_date', 'update')
+    list_editable = ('doctor', 'direction', 'public_on_home_page', 'slug', 'order')
+    list_display_links = ('get_image', 'title')
+    search_fields = ('title',)
+    list_filter = ('doctor', 'direction', 'is_action', 'is_news', 'public_on_home_page', 'pub_date', 'update')
+    save_as = True
+    save_on_top = True
+
+    def get_image(self, obj):
+        return mark_safe(f'<img src="{obj.image}" height=50')
+
+    get_image.short_description = 'Фото'
+
+
+
